8bde046663a2edc16d8500e0b74566539548b1a7
HasanHuseyinGumustas <hasangumustas@gmail.com>
2021-07-09 17:47:46 +0300

    component change 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,25 @@ import { Component } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
-export interface Item { name: string; }
+export interface Item { id: string; name: string; }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  private itemsCollection: AngularFirestoreCollection<Item>;
-  items: Observable<Item[]>;
-  constructor(private afs: AngularFirestore) {
-    this.itemsCollection = afs.collection<Item>('items');
-    this.items = this.itemsCollection.valueChanges();
+
+  export class AppComponent {
+    private itemsCollection: AngularFirestoreCollection<Item>;
+    items: Observable<Item[]>;
+    constructor(private readonly afs: AngularFirestore) {
+      this.itemsCollection = afs.collection<Item>('heroes');
+      this.items = this.itemsCollection.valueChanges({ idField: 'customID' });
   }
-  addItem(item: Item) {
-    this.itemsCollection.add(item);
-  }
+  addItem(name: string) {
+    // Persist a document id
+    const id = this.afs.createId();
+    const item: Item = { id, name };
+    this.itemsCollection.doc(id).set(item);
 }
+  }
